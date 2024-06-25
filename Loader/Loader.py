e070479b004e131afe1ed5de68c4b8ef7dab1b73
abdanhafidz <abdan.hafidz@gmail.com>
2024-06-26 02:06:10 +0700

    feat:fix handler Nodes DB

--- Loader/Loader.py
+++ Loader/Loader.py
@@ -1,5 +1,5 @@
 
-from Controller import NodeController, TransactionController, kontoljembut
+from Controller import BlockChainDisplay, NodeController, TransactionController
 from View import MenuView
 import os
 def LoadView(loc):
@@ -13,7 +13,7 @@ def LoadView(loc):
             case 2:
                 NodeController.Control()
             case 3:
-                kontoljembut.Control()
+                BlockChainDisplay.Control()
             case _:
                 print("Error : Invalid Menu Command")
                 NodeController.Control()
